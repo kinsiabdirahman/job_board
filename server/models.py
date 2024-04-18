25b20720608df7dc5b28feda066de1ec1a462602
mahdiyusuf <mahdiosmanu78@gmail.com>
2024-04-18 20:47:47 +0300

    project

--- server/models.py
+++ server/models.py
@@ -1,36 +1,48 @@
+from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy_serializer import SerializerMixin
-from sqlalchemy.ext.associationproxy import association_proxy
 
-from config import db
+db = SQLAlchemy()
 
-# Models go here!
-class User_table(db.Model):
-    __tablename__ = 'user_tables'
+class User(db.Model, SerializerMixin):
+    _tablename_ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String, unique=True)
+    email = db.Column(db.String, unique=True)
+    password = db.Column(db.String)
+    applications = db.relationship('Application', backref='user', lazy=True)
+
+    def _repr_(self):
+        return f'<User_table {self.id}, {self.username}, {self._password_hash}>'
+
+
+
+class Application(db.Model, SerializerMixin):
+    _tablename_ = 'applications'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    email = db.Coulmn(db.String)
-
-
-class Application(db.Model):
-    __tablename__ = 'applications'
-
-    id = db.Column(db.Integer, primary_key=True)
-    first_name = db.column(db.String)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    job_id = db.Column(db.Integer, db.ForeignKey('jobs.id'))
+    first_name = db.Column(db.String)
     last_name = db.Column(db.String)
     highest_qualification = db.Column(db.String)
-    years_of_experience = db.Coulmn(db.Integer)
-    acdemic_qulification = db.Column(db.String)
+    years_of_experience = db.Column(db.Integer)
+    academic_history = db.Column(db.String)
     work_experience = db.Column(db.String)
 
-class Jobs(db.Model):
-    __table__ = 'jobs'
+    def _repr_(self):
+        return f'<Application {self.id}, {self.first_name}, {self.last_name}, {self.highest_qualification}, {self.years_of_experience}, {self.academic_history}, {self.work_experience}, {self.job_id}, {self.user_id}>'
+
+
+class Job(db.Model, SerializerMixin):
+    _tablename_ = 'jobs'
 
     id = db.Column(db.Integer, primary_key=True)
-    job_tittle = db.Column(db.String)
-    job_description = db.Coulmn(db.String)
+    job_title = db.Column(db.String)
+    job_description = db.Column(db.String)
     job_responsibilities = db.Column(db.String)
     job_salary = db.Column(db.Integer)
-    
+    applications = db.relationship('Application', backref='job', lazy=True)
 
-
+    def _repr_(self):
+        return f'<Job {self.id}, {self.job_title}, {self.job_description}, {self.job_responsibilities}, {self.job_salary}, {self.application_id}>'
