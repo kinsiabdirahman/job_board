6c64f75fce213a8ae1d76d34cb7b3b6b28985668
michelleresian <michellresian@gmail.com>
2024-04-19 10:44:37 +0300

    new branch

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -10,11 +10,6 @@ import SignupForm from "./SignupForm";
 import Home from "./Home";
 import JobDescription from "./JobDescription";
 import ApplicationForm from "./ApplicationForm";
-import Jobdescription from "./JobDescription";
-import Home from "./Home";
-import { AuthProvider, useAuth } from './AuthContext';
-
-//import Home from "./Home";
 
 function App() {
   const [registered, setRegistered] = React.useState(false);
@@ -40,19 +35,5 @@ function App() {
   );
 }
 
-const ProtectedRoute = () => {
-  const { isAuthenticated } = useAuth();
-  const history = useHistory();
-
-  useEffect(() => {
-    
-    if (!isAuthenticated) {
-      history.push('/login');
-    }
-  }, [isAuthenticated, history]);
-
-  
-  return isAuthenticated ? <Home /> : null;
-};
-
 export default App;
+
