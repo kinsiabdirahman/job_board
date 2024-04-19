95f7f06784506d86f452e7791fd46d3c03ba58f9
michelleresian <michellresian@gmail.com>
2024-04-19 10:05:26 +0300

    job-board

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -10,6 +10,11 @@ import SignupForm from "./SignupForm";
 import Home from "./Home";
 import JobDescription from "./JobDescription";
 import ApplicationForm from "./ApplicationForm";
+import Jobdescription from "./JobDescription";
+import Home from "./Home";
+import { AuthProvider, useAuth } from './AuthContext';
+
+//import Home from "./Home";
 
 function App() {
   const [registered, setRegistered] = React.useState(false);
@@ -34,5 +39,20 @@ function App() {
     </Router>
   );
 }
+
+const ProtectedRoute = () => {
+  const { isAuthenticated } = useAuth();
+  const history = useHistory();
+
+  useEffect(() => {
+    
+    if (!isAuthenticated) {
+      history.push('/login');
+    }
+  }, [isAuthenticated, history]);
+
+  
+  return isAuthenticated ? <Home /> : null;
+};
 
 export default App;
